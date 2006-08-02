cdb1bf0015e838ae2ecdfef27f56f41c5a3a9b56
Deneys S. Maartens <dsm@tlabs.ac.za>
2006-08-02 12:50:22 +0000

    Replace seekg() with an is.get() in a for loop.  The gzstream library, for example, does not implement seekg().  A negative seek offset indicates a problem in this particular context, in anycase.
    Also general typographic cleanup.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@18 7b7aedef-af05-4ae1-8915-7847460b892b

--- omtp-ids/FileHeader.cc
+++ omtp-ids/FileHeader.cc
@@ -1,30 +1,21 @@
-// $Id$
-//
-// NAME:
-//   FileHeader -
-//
-// AUTHOR:        Deneys Maartens
-// VERSION:       $Rev$
-// DATE:          $Date: 2004/07/16 11:05:26 $
-// COPYRIGHT:     Deneys Maartens (C) 2004
-//
-// LICENCE:
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of the GNU General Public License as
-//   published by the Free Software Foundation; either version 2, or (at
-//   your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful, but
-//   WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//   General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-//   02111-1307, USA.
-//
-// *********************************************************************
+/* $Id$
+ * Copyright: (C) 2004, 2005, 2006 Deneys S. Maartens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * USA.
+ */
 
 // SYSTEM INCLUDES
 //
@@ -32,16 +23,17 @@
 
 // PROJECT INCLUDES
 //
+#include "ByteSex.hh"
 
 // LOCAL INCLUDES
 //
-#include "ByteSex.hh"
 #include "FileHeader.hh" // class implemented
 
-// LOCAL CONSTANTS
+// FORWARD REFERENCES
 //
-
 using namespace omtp_ids;
+
+// *********************************************************************
 
 // **************************** PRIVATE    *****************************
 
@@ -51,38 +43,38 @@ using namespace omtp_ids;
 
 // ============================ LIFECYCLE  =============================
 
-FileHeader::FileHeader()
- : m_no_records(0),
+FileHeader::FileHeader() :
+	m_no_records(0),
 
-   m_year(0),
-   m_julian_day(0),
-   m_hour(0),
-   m_minute(0),
+	m_year(0),
+	m_julian_day(0),
+	m_hour(0),
+	m_minute(0),
 
-   m_process_year(0),
-   m_process_julian_day(0),
-   m_process_hour(0),
-   m_process_minute(0),
+	m_process_year(0),
+	m_process_julian_day(0),
+	m_process_hour(0),
+	m_process_minute(0),
 
-   m_no_channels(0),
-   m_vis_id(0),
-   m_wv_id(0),
-   m_ir_id(0),
+	m_no_channels(0),
+	m_vis_id(0),
+	m_wv_id(0),
+	m_ir_id(0),
 
-   m_satellite_id(0),
-   m_record_length(0),
+	m_satellite_id(0),
+	m_record_length(0),
 
-   m_no_vis_average(0),
-   m_no_wv_average(0),
-   m_no_ir_average(0),
-   m_averaging_type(0),
+	m_no_vis_average(0),
+	m_no_wv_average(0),
+	m_no_ir_average(0),
+	m_averaging_type(0),
 
-   m_sample_interval(0),
-   m_ir_calibration(0),
-   m_wv_calibration(0),
-   m_fine_adjustment(0),
-   m_ir_space_count(0),
-   m_wv_space_count(0)
+	m_sample_interval(0),
+	m_ir_calibration(0),
+	m_wv_calibration(0),
+	m_fine_adjustment(0),
+	m_ir_space_count(0),
+	m_wv_space_count(0)
 {
 	memcpy(m_fortran, FORTRAN, FORTRAN_LEN);
 	memcpy(m_satellite, SATELLITE, SATELLITE_LEN + 1);
@@ -130,8 +122,9 @@ operator<<(std::ostream& os,
 
 	const int offset = fileheader.record_length()
 		+ FORTRAN_LEN - FILEHEADER_LEN;
-	for (int i = 0; i < offset; ++i)
+	for (int i = 0; i < offset; i++) {
 		os.put('\0');
+	}
 
 	return os;
 }
@@ -179,7 +172,14 @@ operator>>(std::istream& is,
 
 	const int offset = fileheader.record_length()
 		+ FORTRAN_LEN - FILEHEADER_LEN;
-	is.seekg(offset, std::ios::cur);
+
+	if (offset < 0) {
+		is.setstate(std::ios::failbit);
+	}
+	// no seekg() available on gzstream
+	for (int i = 0; i < offset; i++) {
+		is.get();
+	}
 
 	return is;
 }
@@ -189,43 +189,50 @@ operator>>(std::istream& is,
 std::ostream&
 FileHeader::debug(std::ostream& os) const
 {
-	os << "FileHeader :" << std::endl;
-	os << "  fortran bytes        : \"";
-	for (int i = 0; i < FORTRAN_LEN; ++i)
+	os << "FileHeader :";
+	os << "\n  fortran bytes        : \"";
+	for (int i = 0; i < FORTRAN_LEN; i++) {
 		os << static_cast<int>(m_fortran[i]);
-	os << "\"" << std::endl;
+	}
+	os << "\"";
 
-	os << "  number of records    : " << m_no_records         << std::endl;
-	os << "  year                 : " << m_year               << std::endl;
-	os << "  julian_day           : " << m_julian_day         << std::endl;
-	os << "  hour                 : " << m_hour               << std::endl;
-	os << "  minute               : " << m_minute             << std::endl;
+	os << "\n  number of records    : " << m_no_records;
+	os << "\n  year                 : " << m_year;
+	os << "\n  julian_day           : " << m_julian_day;
+	os << "\n  hour                 : " << m_hour;
+	os << "\n  minute               : " << m_minute;
 
-	os << "  process year         : " << m_process_year       << std::endl;
-	os << "  process julian_day   : " << m_process_julian_day << std::endl;
-	os << "  process hour         : " << m_process_hour       << std::endl;
-	os << "  process minute       : " << m_process_minute     << std::endl;
+	os << "\n  process year         : " << m_process_year;
+	os << "\n  process julian_day   : " << m_process_julian_day;
+	os << "\n  process hour         : " << m_process_hour;
+	os << "\n  process minute       : " << m_process_minute;
 
-	os << "  number of channels   : " << m_no_channels        << std::endl;
-	os << "  vis channel id       : " << m_vis_id             << std::endl;
-	os << "  wv channel id        : " << m_wv_id              << std::endl;
-	os << "  ir channel id        : " << m_ir_id              << std::endl;
-	os << "  satellite            : \"";
-	for (int i = 0; i < SATELLITE_LEN; ++i)
+	os << "\n  number of channels   : " << m_no_channels;
+	os << "\n  vis channel id       : " << m_vis_id;
+	os << "\n  wv channel id        : " << m_wv_id;
+	os << "\n  ir channel id        : " << m_ir_id;
+
+	os << "\n  satellite            : \"";
+	for (int i = 0; i < SATELLITE_LEN; i++) {
 		os << m_satellite[i];
-	os << "\"" << std::endl;
-	os << "  satellite id         : " << m_satellite_id       << std::endl;
-	os << "  record length        : " << m_record_length      << std::endl;
-	os << "  vis no average       : " << m_no_vis_average     << std::endl;
-	os << "  ir no average        : " << m_no_ir_average      << std::endl;
-	os << "  wv no average        : " << m_no_wv_average      << std::endl;
-	os << "  averaging type       : " << m_averaging_type     << std::endl;
-	os << "  sample interval      : " << m_sample_interval    << std::endl;
-	os << "  ir calibration       : " << m_ir_calibration     << std::endl;
-	os << "  wv calibration       : " << m_wv_calibration     << std::endl;
-	os << "  fine adjustment      : " << m_fine_adjustment    << std::endl;
-	os << "  ir space count       : " << m_ir_space_count     << std::endl;
-	os << "  wv space count       : " << m_wv_space_count     << std::endl;
+	}
+	os << "\"";
+
+	os << "\n  satellite id         : " << m_satellite_id;
+	os << "\n  record length        : " << m_record_length;
+
+	os << "\n  vis no average       : " << m_no_vis_average;
+	os << "\n  ir no average        : " << m_no_ir_average;
+	os << "\n  wv no average        : " << m_no_wv_average;
+	os << "\n  averaging type       : " << m_averaging_type;
+
+	os << "\n  sample interval      : " << m_sample_interval;
+	os << "\n  ir calibration       : " << m_ir_calibration;
+	os << "\n  wv calibration       : " << m_wv_calibration;
+	os << "\n  fine adjustment      : " << m_fine_adjustment;
+	os << "\n  ir space count       : " << m_ir_space_count;
+	os << "\n  wv space count       : " << m_wv_space_count;
+	os << '\n';
 
 	return os;
 }
--- omtp-ids/ScanLine.cc
+++ omtp-ids/ScanLine.cc
@@ -1,30 +1,21 @@
-// $Id$
-//
-// NAME:
-//   ScanLine -
-//
-// AUTHOR:        Deneys Maartens
-// VERSION:       $Rev$
-// DATE:          $Date: 2004/07/16 11:05:26 $
-// COPYRIGHT:     Deneys Maartens (C) 2004
-//
-// LICENCE:
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of the GNU General Public License as
-//   published by the Free Software Foundation; either version 2, or (at
-//   your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful, but
-//   WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//   General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-//   02111-1307, USA.
-//
-// *********************************************************************
+/* $Id$
+ * Copyright: (C) 2004, 2005, 2006 Deneys S. Maartens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * USA.
+ */
 
 // SYSTEM INCLUDES
 //
@@ -37,9 +28,8 @@
 //
 #include "ScanLine.hh" // class implemented
 
-// LOCAL CONSTANTS
+// FORWARD REFERENCES
 //
-
 using namespace omtp_ids;
 
 // **************************** PRIVATE    *****************************
@@ -60,13 +50,15 @@ operator<<(std::ostream& os,
 
 	const unsigned no_pixels = scanline.m_linepixel.size();
 
-	for (unsigned i = 0; i < no_pixels; ++i)
+	for (unsigned i = 0; i < no_pixels; i++) {
 		os.put(scanline.m_linepixel[i]);
+	}
 
-	int offset = scanline.m_lineheader.length()
+	const int offset = scanline.m_lineheader.length()
 		- no_pixels - LINEHEADER_LEN;
-	for (int i = 0; i < offset; ++i)
+	for (int i = 0; i < offset; i++) {
 		os.put('\0');
+	}
 
 	return os;
 }
@@ -78,21 +70,30 @@ operator>>(std::istream& is,
 	scanline = ScanLine();
 
 	is >> scanline.m_lineheader;
-	if (!is.good())
+	if (!is.good()) {
 		throw "failure while reading line header\n";
+	}
 
 	const int no_pixels = scanline.m_lineheader.no_pixels();
 	scanline.m_linepixel.resize(no_pixels);
 
-	for (int i = 0; i < no_pixels; ++i) {
+	for (int i = 0; i < no_pixels; i++) {
 		scanline.m_linepixel[i] = is.get();
-		if (!is.good())
+		if (!is.good()) {
 			throw "failure while reading line pixel\n";
+		}
 	}
 
-	int offset = scanline.m_lineheader.length()
+	const int offset = scanline.m_lineheader.length()
 		- no_pixels - LINEHEADER_LEN;
-	is.seekg(offset, std::ios::cur);
+
+	if (offset < 0) {
+		is.setstate(std::ios::failbit);
+	}
+	// no seekg() available on gzstream
+	for (int i = 0; i < offset; i++) {
+		is.get();
+	}
 
 	return is;
 }
@@ -102,8 +103,9 @@ operator>>(std::istream& is,
 std::ostream&
 ScanLine::debug(std::ostream& os) const
 {
-	os << "ScanLine :" << std::endl;
-	os << "  number of linepixels : " << m_linepixel.size() << std::endl;
+	os << "ScanLine :";
+	os << "\n  number of linepixels : " << m_linepixel.size();
+	os << "\n";
 	m_lineheader.debug(os);
 	return os;
 }
