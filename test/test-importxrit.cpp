56ac6baddc08ebcb113a8356561b308c1356d49c
Enrico Zini <enrico@enricozini.org>
2006-09-28 16:37:17 +0000

    r1813@viaza:  enrico | 2006-09-28 18:30:46 +0200  Added grib recoding tests to importxrit (warning: they use a huge amount of ram)
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@56 7b7aedef-af05-4ae1-8915-7847460b892b

--- test/test-importxrit.cpp
+++ test/test-importxrit.cpp
@@ -127,6 +127,87 @@ void to::test<3>()
 	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
 }
 
+// Import a full XRIT product and pass it from grib
+template<> template<>
+void to::test<4>()
+{
+	XRITImportOptions opts;
+	opts.directory = "data";
+	opts.resolution = "H";
+	opts.productid1 = "MSG1";
+	opts.productid2 = "HRV";
+	opts.timing = "200604261945";
+	opts.subarea = false;
+	std::auto_ptr<Image> img = importXRIT(opts);
+	img = recodeThroughGrib(*img);
+
+	//gen_ensure_equals(img->name, ""); // unverified
+	gen_ensure_equals(img->data->columns, 5568);
+	gen_ensure_equals(img->data->lines, 11136);
+	gen_ensure_equals(img->data->slope, 1);
+	gen_ensure_equals(img->data->offset, 0);
+	gen_ensure_equals(img->year, 2006);
+	gen_ensure_equals(img->month, 4);
+	gen_ensure_equals(img->day, 26);
+	gen_ensure_equals(img->hour, 19);
+	gen_ensure_equals(img->minute, 45);
+	gen_ensure_equals(img->sublon, 0);
+	gen_ensure_equals(img->channel_id, 12);
+	gen_ensure_equals(img->spacecraft_id, 321); // unverified
+	gen_ensure_equals(img->column_factor, -40927014);
+	gen_ensure_equals(img->line_factor, -40927014);
+	gen_ensure_equals(img->column_offset, -2);
+	gen_ensure_equals(img->line_offset, 462);
+	gen_ensure_equals(img->data->bpp, 32); // unverified
+	gen_ensure_equals(img->data->unscaled(0, 0), 0); // unverified
+	gen_ensure_equals(img->data->unscaled(10, 10), 0); // unverified
+	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
+	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
+}
+
+// Import a subarea of an XRIT product and pass it from grib
+template<> template<>
+void to::test<5>()
+{
+	XRITImportOptions opts;
+	opts.directory = "data";
+	opts.resolution = "H";
+	opts.productid1 = "MSG1";
+	opts.productid2 = "HRV";
+	opts.timing = "200604261945";
+	opts.subarea = true;
+	opts.AreaLinStart = 2000;
+	opts.AreaNlin = 300;
+	opts.AreaPixStart = 1000;
+	opts.AreaNpix = 400;
+
+	std::auto_ptr<Image> img = importXRIT(opts);
+	img = recodeThroughGrib(*img);
+
+	//gen_ensure_equals(img->name, ""); // unverified
+	gen_ensure_equals(img->data->columns, 400);
+	gen_ensure_equals(img->data->lines, 300);
+	gen_ensure_equals(img->data->slope, 1);
+	gen_ensure_equals(img->data->offset, 0);
+	gen_ensure_equals(img->year, 2006);
+	gen_ensure_equals(img->month, 4);
+	gen_ensure_equals(img->day, 26);
+	gen_ensure_equals(img->hour, 19);
+	gen_ensure_equals(img->minute, 45);
+	gen_ensure_equals(img->sublon, 0);
+	gen_ensure_equals(img->channel_id, 12);
+	gen_ensure_equals(img->spacecraft_id, 321); // unverified
+	gen_ensure_equals(img->column_factor, -40927014);
+	gen_ensure_equals(img->line_factor, -40927014);
+	gen_ensure_equals(img->column_offset, 998);
+	gen_ensure_equals(img->line_offset, 2462);
+	gen_ensure_equals(img->data->bpp, 32); // unverified
+	gen_ensure_equals(img->data->unscaled(0, 0), 0); // unverified
+	gen_ensure_equals(img->data->unscaled(10, 10), 0); // unverified
+	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
+	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
+}
+
 }
 
 /* vim:set ts=4 sw=4: */
