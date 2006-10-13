9ad0dd88533acf83a832e8397b7685d561272b1b
Enrico Zini <enrico@enricozini.org>
2006-10-13 12:50:26 +0000

    r1920@viaza:  enrico | 2006-10-13 12:35:19 +0200  Added more (unverified) channel information
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@93 7b7aedef-af05-4ae1-8915-7847460b892b

--- conv/Image.cpp
+++ conv/Image.cpp
@@ -16,10 +16,19 @@ using namespace std;
 static struct ChannelInfo {
 	size_t decimalDigits;
 } channelInfo[] = {
-	{ 2 },	// 0
-	{ 2 },	// 1
-	{ 2 },	// 2
-	{ 2 },	// 3
+	{ 2 },	//  0
+	{ 2 },	//  1
+	{ 2 },	//  2
+	{ 2 },	//  3
+	{ 2 },	//  4
+	{ 2 },	//  5
+	{ 2 },	//  6
+	{ 2 },	//  7
+	{ 2 },	//  8
+	{ 2 },	//  9
+	{ 2 },	// 10
+	{ 2 },	// 11
+	{ 2 },	// 12
 };
 
 static const size_t channel_info_size = sizeof(channelInfo) / sizeof(struct ChannelInfo);
