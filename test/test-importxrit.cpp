581836158cc5bea880039c51815e5f171cc6eb3b
Enrico Zini <enrico@enricozini.org>
2006-10-13 12:52:20 +0000

    r1927@viaza:  enrico | 2006-10-13 14:07:33 +0200  Cleaned up XRIT tests and added recoding to/from NetCDF
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@100 7b7aedef-af05-4ae1-8915-7847460b892b

--- test/test-importxrit.cpp
+++ test/test-importxrit.cpp
@@ -22,6 +22,8 @@
 #include "test-utils.h"
 #include <ImportXRIT.h>
 
+#undef PERFORM_SLOW_TESTS
+
 using namespace msat;
 
 namespace tut {
@@ -37,6 +39,61 @@ struct importxrit_shar
 	}
 };
 TESTGRP(importxrit);
+
+static void checkGeneralImageData(Image& img)
+{
+	gen_ensure_equals(img.year, 2006);
+	gen_ensure_equals(img.month, 4);
+	gen_ensure_equals(img.day, 26);
+	gen_ensure_equals(img.hour, 19);
+	gen_ensure_equals(img.minute, 45);
+	gen_ensure_equals(img.sublon, 0);
+	gen_ensure_equals(img.channel_id, 12);
+	gen_ensure_equals(img.spacecraft_id, 55);
+}
+
+static void checkFullImageData(Image& img)
+{
+	checkGeneralImageData(img);
+
+	gen_ensure_equals(img.data->columns, 5568);
+	gen_ensure_equals(img.data->lines, 11136);
+	gen_ensure_equals(img.column_offset, -2);
+	gen_ensure_equals(img.line_offset, 462);
+	gen_ensure_equals(img.data->scaled(0, 0), 0); // unverified
+	gen_ensure_equals(img.data->scaled(10, 10), 0); // unverified
+	gen_ensure_similar(img.data->scaled(356, 5569),  9.4077949523, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(357, 5569),  7.2958407402, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(358, 5569),  5.7278747558, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(214, 5570), 12.2877311706, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(215, 5570), 10.8157634735, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(216, 5570), 11.0077600479, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(466, 5571),  7.4878363609, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(467, 5571),  8.3198184967, 0.001); // unverified
+	gen_ensure_similar(img.data->scaled(468, 5571),  8.8318071365, 0.001); // unverified
+}
+
+static void checkCroppedImageData(Image& img)
+{
+	checkGeneralImageData(img);
+
+	gen_ensure_equals(img.data->columns, 400);
+	gen_ensure_equals(img.data->lines, 300);
+	gen_ensure_equals(img.column_offset, 98);
+	gen_ensure_equals(img.line_offset, 5962);
+	gen_ensure_equals(img.data->scaled(0, 0), 0); // unverified
+	gen_ensure_equals(img.data->scaled(10, 10), 0); // unverified
+	gen_ensure_similar(img.data->scaled(256, 69),  9.40779495239, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(257, 69),  7.29584074020, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(258, 69),  5.72787475585, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(114, 70), 12.28773117065, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(115, 70), 10.81576347351, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(116, 70), 11.00776004791, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(366, 71),  7.48783636093, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(367, 71),  8.31981849670, 0.01); // unverified
+	gen_ensure_similar(img.data->scaled(368, 71),  8.83180713653, 0.01); // unverified
+}
+
 
 // Test the isXRIT function
 template<> template<>
@@ -62,35 +119,16 @@ void to::test<2>()
 	std::auto_ptr<Image> img = importXRIT(opts);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 5568);
-	gen_ensure_equals(img->data->lines, 11136);
-	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
-	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55);
 	gen_ensure_equals(img->column_factor, -40927014);
 	gen_ensure_equals(img->line_factor, -40927014);
-	gen_ensure_equals(img->column_offset, -2);
-	gen_ensure_equals(img->line_offset, 462);
+	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
 	gen_ensure_equals(img->data->bpp, 10);
 	gen_ensure_equals(img->data->scalesToInt, true);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
-	gen_ensure_equals(img->data->scaled(356, 5569),  9.40779495239257812500); // unverified
-	gen_ensure_equals(img->data->scaled(357, 5569),  7.29584074020385742188); // unverified
-	gen_ensure_equals(img->data->scaled(358, 5569),  5.72787475585937500000); // unverified
-	gen_ensure_equals(img->data->scaled(214, 5570), 12.28773117065429687500); // unverified
-	gen_ensure_equals(img->data->scaled(215, 5570), 10.81576347351074218750); // unverified
-	gen_ensure_equals(img->data->scaled(216, 5570), 11.00776004791259765625); // unverified
-	gen_ensure_equals(img->data->scaled(466, 5571),  7.48783636093139648438); // unverified
-	gen_ensure_equals(img->data->scaled(467, 5571),  8.31981849670410156250); // unverified
-	gen_ensure_equals(img->data->scaled(468, 5571),  8.83180713653564453125); // unverified
+
+	test_tag("fullXRIT");
+	checkFullImageData(*img);
+	test_untag();
 
 #if 0
 	-- look for nonempty values
@@ -121,42 +159,23 @@ void to::test<3>()
 	std::auto_ptr<Image> img = importXRIT(opts);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 400);
-	gen_ensure_equals(img->data->lines, 300);
-	gen_ensure_similar(img->data->slope, 0.0319993, 0.00001);
-	gen_ensure_similar(img->data->offset, -1.6319643, 0.00001);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55); // unverified
 	gen_ensure_equals(img->column_factor, -40927014);
 	gen_ensure_equals(img->line_factor, -40927014);
-	gen_ensure_equals(img->column_offset, 98);
-	gen_ensure_equals(img->line_offset, 5962);
+	gen_ensure_similar(img->data->slope, 0.0319993, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.6319643, 0.00001);
 	gen_ensure_equals(img->data->bpp, 10); // unverified
 	gen_ensure_equals(img->data->scalesToInt, true);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
-	gen_ensure_equals(img->data->scaled(256, 69),  9.40779495239257812500); // unverified
-	gen_ensure_equals(img->data->scaled(257, 69),  7.29584074020385742188); // unverified
-	gen_ensure_equals(img->data->scaled(258, 69),  5.72787475585937500000); // unverified
-	gen_ensure_equals(img->data->scaled(114, 70), 12.28773117065429687500); // unverified
-	gen_ensure_equals(img->data->scaled(115, 70), 10.81576347351074218750); // unverified
-	gen_ensure_equals(img->data->scaled(116, 70), 11.00776004791259765625); // unverified
-	gen_ensure_equals(img->data->scaled(366, 71),  7.48783636093139648438); // unverified
-	gen_ensure_equals(img->data->scaled(367, 71),  8.31981849670410156250); // unverified
-	gen_ensure_equals(img->data->scaled(368, 71),  8.83180713653564453125); // unverified
+
+	test_tag("croppedXRIT");
+	checkCroppedImageData(*img);
+	test_untag();
 }
 
 // Import a full XRIT product and pass it from grib
 template<> template<>
 void to::test<4>()
 {
-#if 0
+#if PERFORM_SLOW_TESTS
 	-- Temporarily disabled as it currently uses too much ram
 
 	XRITImportOptions opts;
@@ -170,26 +189,16 @@ void to::test<4>()
 	img = recodeThroughGrib(*img);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 5568);
-	gen_ensure_equals(img->data->lines, 11136);
+	gen_ensure_equals(img->column_factor, Image::columnFactorFromSeviriDX(Image::seviriDXFromColumnFactor(-40927014)));
+	gen_ensure_equals(img->line_factor, Image::lineFactorFromSeviriDY(Image::seviriDYFromLineFactor(-40927014)));
 	gen_ensure_equals(img->data->slope, 1);
 	gen_ensure_equals(img->data->offset, 0);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55); // unverified
-	gen_ensure_equals(img->column_factor, -40927014);
-	gen_ensure_equals(img->line_factor, -40927014);
-	gen_ensure_equals(img->column_offset, -2);
-	gen_ensure_equals(img->line_offset, 462);
 	gen_ensure_equals(img->data->bpp, 32); // unverified
 	gen_ensure_equals(img->data->scalesToInt, false);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
+
+	test_tag("fullXRITRecodedGrib");
+	checkFullImageData(*img);
+	test_untag();
 
 	gen_ensure_imagedata_similar(*img->data, *imgs[0]->data, 0.0001);
 #endif
@@ -215,35 +224,16 @@ void to::test<5>()
 	std::auto_ptr<Image> img = recodeThroughGrib(*imgorig);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 400);
-	gen_ensure_equals(img->data->lines, 300);
-	gen_ensure_equals(img->data->slope, 0.01);
-	gen_ensure_equals(img->data->offset, 0);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55); // unverified
 	gen_ensure_equals(img->column_factor, Image::columnFactorFromSeviriDX(Image::seviriDXFromColumnFactor(-40927014)));
 	gen_ensure_equals(img->line_factor, Image::lineFactorFromSeviriDY(Image::seviriDYFromLineFactor(-40927014)));
-	gen_ensure_equals(img->column_offset, 98);
-	gen_ensure_equals(img->line_offset, 5962);
+	gen_ensure_equals(img->data->slope, 0.01);
+	gen_ensure_equals(img->data->offset, 0);
 	gen_ensure_equals(img->data->bpp, 11); // unverified
 	gen_ensure_equals(img->data->scalesToInt, true);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
-	gen_ensure_similar(img->data->scaled(256, 69),  9.41, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(257, 69),  7.30, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(258, 69),  5.73, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(114, 70), 12.29, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(115, 70), 10.82, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(116, 70), 11.01, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(366, 71),  7.49, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(367, 71),  8.32, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(368, 71),  8.83, 0.01); // unverified
+
+	test_tag("croppedXRITRecodedGrib");
+	checkCroppedImageData(*img);
+	test_untag();
 
 	gen_ensure_imagedata_similar(*img->data, *imgorig->data, 0.01);
 }
@@ -252,7 +242,7 @@ void to::test<5>()
 template<> template<>
 void to::test<6>()
 {
-#if 0
+#if PERFORM_SLOW_TESTS
 	-- Temporarily disabled as it currently uses too much ram
 
 	XRITImportOptions opts;
@@ -266,26 +256,16 @@ void to::test<6>()
 	img = recodeThroughNetCDF24(*img);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 5568);
-	gen_ensure_equals(img->data->lines, 11136);
-	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
-	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55); // unverified
 	gen_ensure_equals(img->column_factor, -40927014);
 	gen_ensure_equals(img->line_factor, -40927014);
-	gen_ensure_equals(img->column_offset, -2);
-	gen_ensure_equals(img->line_offset, 462);
+	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
 	gen_ensure_equals(img->data->bpp, 32); // unverified
 	gen_ensure_equals(img->data->scalesToInt, false);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
+
+	test_tag("fullXRITRecodedNetCDF24");
+	checkFullImageData(*img);
+	test_untag();
 
 	gen_ensure_imagedata_similar(*img->data, *imgs[0]->data, 0.0001);
 #endif
@@ -311,39 +291,86 @@ void to::test<7>()
 	std::auto_ptr<Image> img = recodeThroughNetCDF24(*imgorig);
 
 	//gen_ensure_equals(img->name, ""); // unverified
-	gen_ensure_equals(img->data->columns, 400);
-	gen_ensure_equals(img->data->lines, 300);
-	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
-	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
-	gen_ensure_equals(img->year, 2006);
-	gen_ensure_equals(img->month, 4);
-	gen_ensure_equals(img->day, 26);
-	gen_ensure_equals(img->hour, 19);
-	gen_ensure_equals(img->minute, 45);
-	gen_ensure_equals(img->sublon, 0);
-	gen_ensure_equals(img->channel_id, 12);
-	gen_ensure_equals(img->spacecraft_id, 55); // unverified
 	gen_ensure_equals(img->column_factor, Image::columnFactorFromSeviriDX(Image::seviriDXFromColumnFactor(-40927014)));
 	gen_ensure_equals(img->line_factor, Image::lineFactorFromSeviriDY(Image::seviriDYFromLineFactor(-40927014)));
-	gen_ensure_equals(img->column_offset, 98);
-	gen_ensure_equals(img->line_offset, 5962);
+	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
 	gen_ensure_equals(img->data->bpp, 10);
 	gen_ensure_equals(img->data->scalesToInt, true);
-	gen_ensure_equals(img->data->scaled(0, 0), 0); // unverified
-	gen_ensure_equals(img->data->scaled(10, 10), 0); // unverified
-	gen_ensure_similar(img->data->scaled(256, 69),  9.41, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(257, 69),  7.30, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(258, 69),  5.73, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(114, 70), 12.29, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(115, 70), 10.82, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(116, 70), 11.01, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(366, 71),  7.49, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(367, 71),  8.32, 0.01); // unverified
-	gen_ensure_similar(img->data->scaled(368, 71),  8.83, 0.01); // unverified
+
+	test_tag("croppedXRITRecodedNetCDF24");
+	checkCroppedImageData(*img);
+	test_untag();
 
 	gen_ensure_imagedata_similar(*img->data, *imgorig->data, 0.0001);
 }
 
+// Import a full XRIT product and pass it from netcdf
+template<> template<>
+void to::test<8>()
+{
+#if PERFORM_SLOW_TESTS
+	-- Temporarily disabled as it currently uses too much ram
+
+	XRITImportOptions opts;
+	opts.directory = "data";
+	opts.resolution = "H";
+	opts.productid1 = "MSG1";
+	opts.productid2 = "HRV";
+	opts.timing = "200604261945";
+	opts.subarea = false;
+	std::auto_ptr<Image> img = importXRIT(opts);
+	img = recodeThroughNetCDF(*img);
+
+	//gen_ensure_equals(img->name, ""); // unverified
+	gen_ensure_equals(img->column_factor, -40927014);
+	gen_ensure_equals(img->line_factor, -40927014);
+	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
+	gen_ensure_equals(img->data->bpp, 32); // unverified
+	gen_ensure_equals(img->data->scalesToInt, false);
+
+	test_tag("fullXRITRecodedNetCDF");
+	checkFullImageData(*img);
+	test_untag();
+
+	gen_ensure_imagedata_similar(*img->data, *imgs[0]->data, 0.0001);
+#endif
+}
+
+// Import a subarea of an XRIT product and pass it from netcdf
+template<> template<>
+void to::test<9>()
+{
+	XRITImportOptions opts;
+	opts.directory = "data";
+	opts.resolution = "H";
+	opts.productid1 = "MSG1";
+	opts.productid2 = "HRV";
+	opts.timing = "200604261945";
+	opts.subarea = true;
+	opts.AreaLinStart = 5500;
+	opts.AreaNlin = 300;
+	opts.AreaPixStart = 100;
+	opts.AreaNpix = 400;
+
+	std::auto_ptr<Image> imgorig = importXRIT(opts);
+	std::auto_ptr<Image> img = recodeThroughNetCDF(*imgorig);
+
+	//gen_ensure_equals(img->name, ""); // unverified
+	gen_ensure_equals(img->column_factor, Image::columnFactorFromSeviriDX(Image::seviriDXFromColumnFactor(-40927014)));
+	gen_ensure_equals(img->line_factor, Image::lineFactorFromSeviriDY(Image::seviriDYFromLineFactor(-40927014)));
+	gen_ensure_similar(img->data->slope, 0.031999f, 0.00001);
+	gen_ensure_similar(img->data->offset, -1.63196f, 0.00001);
+	gen_ensure_equals(img->data->bpp, 10);
+	gen_ensure_equals(img->data->scalesToInt, true);
+
+	test_tag("croppedXRITRecodedNetCDF");
+	checkCroppedImageData(*img);
+	test_untag();
+
+	gen_ensure_imagedata_similar(*img->data, *imgorig->data, 0.0001);
+}
 }
 
 /* vim:set ts=4 sw=4: */
