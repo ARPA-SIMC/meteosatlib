36b4d8dc68c01ee183ace40673f71c1d05a74286
Enrico Zini <enrico@enricozini.org>
2006-11-13 15:18:55 +0000

    r2046@viaza:  enrico | 2006-11-13 16:18:25 +0100  Get area coordinates as line first then col
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@142 7b7aedef-af05-4ae1-8915-7847460b892b

--- tools/msatconv.cpp
+++ tools/msatconv.cpp
@@ -74,7 +74,7 @@ void do_help(const char* argv0, ostream& out)
       << "  --netcdf         Convert to NetCDF" << endl
       << "  --netcdf24       Convert to NetCDF" << endl
 #endif
-      << "  --area='x,y,w,h' Crop the source image(s) to the given area" << endl;
+      << "  --area='line,col,height,width' Crop the source image(s) to the given area" << endl;
 }
 void usage(char *pname)
 {
@@ -185,9 +185,9 @@ int main( int argc, char* argv[] )
 				break;
 #endif
 			case 'a':
-				if (sscanf(optarg, "%d,%d,%d,%d", &ax,&ay,&aw,&ah) != 4)
+				if (sscanf(optarg, "%d,%d,%d,%d", &ay,&ax,&ah,&aw) != 4)
 				{
-					cerr << "Area value should be in the format x,y,width,height" << endl;
+					cerr << "Area value should be in the format line,col,height,width" << endl;
 					do_help(argv[0], cerr);
 					return 1;
 				}
