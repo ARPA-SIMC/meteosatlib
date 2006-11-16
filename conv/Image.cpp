b806c0865209eea7d771d1cc87782d4f0ece7b31
Enrico Zini <enrico@enricozini.org>
2006-11-16 14:07:52 +0000

    r2108@viaza:  enrico | 2006-11-16 14:38:47 +0100  Updated per-channel decimal digits table
    git-svn-id: svn+ssh://svn.code.sf.net/p/meteosatlib/code/trunk@173 7b7aedef-af05-4ae1-8915-7847460b892b

--- conv/Image.cpp
+++ conv/Image.cpp
@@ -20,18 +20,18 @@ static struct ChannelInfo {
 	size_t decimalDigits;
 } channelInfo[] = {
 	{ 2 },	//  0		FIXME: unverified
-	{ 2 },	//  1		FIXME: unverified
-	{ 2 },	//  2		FIXME: unverified
-	{ 2 },	//  3		FIXME: unverified
-	{ 2 },	//  4		FIXME: unverified
-	{ 2 },	//  5		FIXME: unverified
-	{ 2 },	//  6		FIXME: unverified
-	{ 2 },	//  7		FIXME: unverified
-	{ 2 },	//  8		FIXME: unverified
-	{ 2 },	//  9		FIXME: unverified
-	{ 2 },	// 10		FIXME: unverified
-	{ 2 },	// 11		FIXME: unverified
-	{ 2 },	// 12		FIXME: unverified
+	{ 4 },	//  1
+	{ 4 },	//  2
+	{ 4 },	//  3
+	{ 2 },	//  4
+	{ 2 },	//  5
+	{ 2 },	//  6
+	{ 2 },	//  7
+	{ 2 },	//  8
+	{ 2 },	//  9
+	{ 2 },	// 10
+	{ 2 },	// 11
+	{ 4 },	// 12
 };
 
 static const size_t channel_info_size = sizeof(channelInfo) / sizeof(struct ChannelInfo);
