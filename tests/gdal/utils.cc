e458f9d081a8a4d3ec97677e541b0c25268996c9
Enrico Zini <enrico@enricozini.org>
2016-04-18 17:55:42 +0200

    Refactored more tests

---
+++ tests/gdal/utils.cc
@@ -0,0 +1,189 @@
+#include "utils.h"
+#include <gdal_priv.h>
+#include <ogr_spatialref.h>
+
+using namespace std;
+
+namespace msat {
+namespace tests {
+
+namespace gdal {
+
+/*
+ * Error handling
+ */
+class gdalexception : public std::exception
+{
+    std::string msg;
+
+public:
+    gdalexception() throw ();
+    gdalexception(CPLErr eErrClass, int err_no, const char *msg) throw ();
+    virtual ~gdalexception() throw () {}
+
+    virtual const char* what() const throw() { return msg.c_str(); }
+};
+
+gdalexception::gdalexception() throw () : msg(CPLGetLastErrorMsg())
+{
+}
+
+gdalexception::gdalexception(CPLErr eErrClass, int err_no, const char *msg) throw ()
+{
+    stringstream fullmsg;
+    fullmsg << "GDAL ";
+    switch (eErrClass)
+    {
+        case CE_None: fullmsg << "(no error)"; break;
+        case CE_Debug: fullmsg << "debug"; break;
+        case CE_Warning: fullmsg << "warning"; break;
+        case CE_Failure: fullmsg << "failure"; break;
+        case CE_Fatal: fullmsg << "fatal"; break;
+        default: fullmsg << "(unknown)"; break;
+    }
+    fullmsg << " error " << err_no << ": " << msg;
+    this->msg = fullmsg.str();
+}
+
+
+static void throwExceptionsOnGDALErrors(CPLErr eErrClass, int err_no, const char *msg)
+{
+    throw gdalexception(eErrClass, err_no, msg);
+}
+
+static bool initialized = false;
+
+bool has_driver(const std::string& name)
+{
+    GDALDriverManager* dm = GetGDALDriverManager();
+    return dm->GetDriverByName(name.c_str()) != 0;
+}
+
+std::unique_ptr<GDALDataset> open_ro(const std::string& name)
+{
+    return std::unique_ptr<GDALDataset>((GDALDataset*)GDALOpen(name.c_str(), GA_ReadOnly));
+}
+
+std::unique_ptr<GDALDataset> recode(GDALDataset* ds, bool leaveFile,
+        const char* driverName, const std::string& opt1, const std::string& opt2, const std::string& opt3)
+{
+    GDALDriver* driver = (GDALDriver*)GDALGetDriverByName(driverName);
+    TempTestFile tf(leaveFile);
+    unique_ptr<GDALDataset> dataset((GDALDataset*)GDALCreateCopy(driver, tf.name().c_str(), ds,
+                TRUE, NULL,
+                GDALDummyProgress, NULL));
+    return dataset;
+}
+
+std::unique_ptr<GDALDataset> recode(GDALDataset* ds, const msat::proj::ImageBox& cropArea, bool leaveFile,
+        const char* driverName, const std::string& opt1, const std::string& opt2, const std::string& opt3)
+{
+    TempTestFile file(leaveFile);
+
+    GDALDriver* driver = GetGDALDriverManager()->GetDriverByName(driverName);
+    wassert(actual(driver != 0).istrue());
+
+    msat::dataset::recode(ds, cropArea, file.name(), driver, opt1, opt2, opt3);
+
+    return open_ro(file.name().c_str());
+}
+
+}
+
+
+TempTestFile::TempTestFile(bool leave) : leave(leave)
+{
+    char* pn = tempnam(".", "test");
+    pathname = pn;
+    free(pn);
+}
+
+
+GeoReferencer::GeoReferencer(GDALDataset* ds)
+    : ds(ds), proj(0), latlon(0), toLatLon(0), fromLatLon(0)
+{
+    if (ds->GetGeoTransform(geoTransform) != CE_None)
+        throw std::runtime_error("no geotransform found in input dataset");
+
+    dataset::invertGeoTransform(geoTransform, invGeoTransform);
+
+    const char* projname = ds->GetProjectionRef();
+    if (!projname || !projname[0])
+        throw std::runtime_error("no projection name found in input dataset");
+
+    projection = projname;
+
+    proj = new OGRSpatialReference(projection.c_str());
+    latlon = proj->CloneGeogCS();
+    toLatLon = OGRCreateCoordinateTransformation(proj, latlon);
+    fromLatLon = OGRCreateCoordinateTransformation(latlon, proj);
+
+    delete proj; proj = 0;
+    delete latlon; latlon = 0;
+}
+
+GeoReferencer::~GeoReferencer()
+{
+    if (proj) delete proj;
+    if (latlon) delete latlon;
+    if (toLatLon) delete toLatLon;
+    if (fromLatLon) delete fromLatLon;
+}
+
+void GeoReferencer::pixelToProjected(int x, int y, double& px, double& py) const
+{
+    px = geoTransform[0] + geoTransform[1] * x
+        + geoTransform[2] * y;
+    py = geoTransform[3] + geoTransform[4] * x
+        + geoTransform[5] * y;
+}
+
+void GeoReferencer::projectedToPixel(double px, double py, int& x, int& y) const
+{
+    x = lrint(invGeoTransform[0] + px * invGeoTransform[1] + py * invGeoTransform[2]);
+    y = lrint(invGeoTransform[3] + px * invGeoTransform[4] + py * invGeoTransform[5]);
+}
+
+void GeoReferencer::projectedToLatlon(double px, double py, double& lat, double& lon)
+{
+    if (!toLatLon->Transform(1, &px, &py))
+        throw std::runtime_error("points failed to transform to lat,lon");
+    lat = py;
+    lon = px;
+}
+
+void GeoReferencer::latlonToProjected(double lat, double lon, double& px, double& py)
+{
+    if (!fromLatLon->Transform(1, &lon, &lat))
+        throw std::runtime_error("points failed to transform from lat,lon");
+    py = lat;
+    px = lon;
+}
+
+GDALFixture::~GDALFixture()
+{
+    delete _dataset;
+}
+
+void GDALFixture::test_setup()
+{
+    if (!gdal::initialized)
+    {
+        GDALAllRegister();
+        CPLSetErrorHandler(gdal::throwExceptionsOnGDALErrors);
+        gdal::initialized = true;
+    }
+
+    if (!gdal::has_driver(driver))
+        throw TestFailed("driver " + driver + " is not available");
+}
+
+GDALDataset* GDALFixture::dataset()
+{
+    if (!_dataset)
+        _dataset = gdal::open_ro(fname).release();
+    return _dataset;
+}
+
+}
+}
---
+++ tests/gdal/utils.h
@@ -0,0 +1,189 @@
+#ifndef TEST_GDAL_UTILS_H
+#define TEST_GDAL_UTILS_H
+
+#include <msat/gdal/const.h>
+#include <msat/gdal/dataset.h>
+#include <msat/utils/tests.h>
+#include <memory>
+
+namespace msat {
+namespace tests {
+
+namespace gdal {
+
+bool has_driver(const std::string& name);
+
+std::unique_ptr<GDALDataset> open_ro(const std::string& name);
+
+static inline int32_t read_int32(GDALRasterBand* rb, int x, int y)
+{
+    int32_t res;
+    rb->RasterIO(GF_Read, x, y, 1, 1, &res, 1, 1, GDT_Int32, 0, 0);
+    return res;
+}
+
+static inline float read_float32(GDALRasterBand* rb, int x, int y)
+{
+    float res;
+    rb->RasterIO(GF_Read, x, y, 1, 1, &res, 1, 1, GDT_Float32, 0, 0);
+    return res;
+}
+
+std::unique_ptr<GDALDataset> recode(GDALDataset* ds, bool leaveFile,
+    const char* driver,
+    const std::string& opt1 = std::string(),
+    const std::string& opt2 = std::string(),
+    const std::string& opt3 = std::string());
+
+std::unique_ptr<GDALDataset> recode(GDALDataset* ds, const msat::proj::ImageBox& cropArea, bool leaveFile,
+    const char* driver,
+    const std::string& opt1 = std::string(),
+    const std::string& opt2 = std::string(),
+    const std::string& opt3 = std::string());
+
+}
+
+// RAII-style class to ensure cleanup of a temporary test file
+class TempTestFile
+{
+    std::string pathname;
+    bool leave;
+
+public:
+    TempTestFile(bool leave = false);
+    TempTestFile(const std::string& pathname, bool leave = false) : pathname(pathname), leave(leave) { unlink(pathname.c_str()); }
+    ~TempTestFile() { if (!leave) unlink(pathname.c_str()); }
+
+    const std::string& name() const { return pathname; }
+};
+
+class GeoReferencer
+{
+protected:
+    GDALDataset* ds;
+    std::string projection;
+    double geoTransform[6];
+    double invGeoTransform[6];
+    OGRSpatialReference* proj;
+    OGRSpatialReference* latlon;
+    OGRCoordinateTransformation* toLatLon;
+    OGRCoordinateTransformation* fromLatLon;
+
+public:
+    GeoReferencer(GDALDataset* ds);
+    ~GeoReferencer();
+
+    void pixelToProjected(int x, int y, double& px, double& py) const;
+    void projectedToPixel(double px, double py, int& x, int& y) const;
+
+    void projectedToLatlon(double px, double py, double& lat, double& lon);
+    void latlonToProjected(double lat, double lon, double& px, double& py);
+
+    void pixelToLatlon(int x, int y, double& lat, double& lon)
+    {
+        double px, py;
+        pixelToProjected(x, y, px, py);
+        projectedToLatlon(px, py, lat, lon);
+    }
+    void latlonToPixel(double lat, double lon, int& x, int& y)
+    {
+        double px, py;
+        latlonToProjected(lat, lon, px, py);
+        projectedToPixel(px, py, x, y);
+    }
+};
+
+struct GDALFixture : public Fixture
+{
+    std::string driver;
+    std::string fname;
+    GDALDataset* _dataset = nullptr;
+
+    GDALFixture(const char* driver, const char* fname)
+        : driver(driver), fname(fname)
+    {
+    }
+    virtual ~GDALFixture();
+
+    void test_setup();
+
+    GDALDataset* dataset();
+
+    virtual msat::proj::ImageBox get_crop_area() { throw std::runtime_error("get_crop_area called on a test fixture where it has not been implemented"); }
+    virtual void check_full_image_data(GDALDataset* dataset) {}
+    virtual void check_cropped_image_data(GDALDataset* dataset) {}
+};
+
+template<typename Fixture>
+struct ImportTest : public FixtureTestCase<Fixture>
+{
+    using FixtureTestCase<Fixture>::FixtureTestCase;
+
+    void register_tests() override
+    {
+        // Test that the image is read with the right driver
+        this->add_method("driver", [](Fixture& f) {
+            wassert(actual(f.dataset() != 0).istrue());
+            wassert(actual(GDALGetDriverShortName(f.dataset()->GetDriver())) == f.driver);
+            wassert(actual(f.dataset()->GetRasterCount()) >= 1);
+        });
+
+        // Test read on full image
+        this->add_method("open_plain", [](Fixture& f) {
+            f.check_full_image_data(f.dataset());
+        });
+
+        // Try CreateCopy with GRIB msat/msat
+        this->add_method("recode_grib_msat", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), false, "MsatGRIB", "TEMPLATE=msat/msat"));
+            f.check_full_image_data(recoded.get());
+        });
+
+        // Try CreateCopy with GRIB msat/ecmwf
+        this->add_method("recode_grib_ecmwf", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), false, "MsatGRIB", "TEMPLATE=msat/ecmwf"));
+            f.check_full_image_data(recoded.get());
+        });
+
+        // Try CreateCopy with NetCDF
+        this->add_method("recode_grib_netcdf", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), false, "MsatNetCDF"));
+            f.check_full_image_data(recoded.get());
+        });
+
+        // Try CreateCopy with NetCDF24
+        this->add_method("recode_grib_netcdf24", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), false, "MsatNetCDF24"));
+            f.check_full_image_data(recoded.get());
+        });
+
+        // Try reimporting a subarea exported to GRIB with the msat/msat template
+        this->add_method("crop_recode_grib_msat", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), f.get_crop_area(), false, "MsatGRIB", "TEMPLATE=msat/msat"));
+            f.check_cropped_image_data(recoded.get());
+        });
+
+        // Try reimporting a subarea exported to GRIB with the msat/ecmwf template
+        this->add_method("crop_recode_grib_ecmwf", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), f.get_crop_area(), false, "MsatGRIB", "TEMPLATE=msat/ecmwf"));
+            f.check_cropped_image_data(recoded.get());
+        });
+
+        // Try reimporting a subarea exported to NetCDF
+        this->add_method("crop_recode_grib_netcdf", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), f.get_crop_area(), false, "MsatNetCDF"));
+            f.check_cropped_image_data(recoded.get());
+        });
+
+        // Try reimporting a subarea exported to NetCDF24
+        this->add_method("crop_recode_grib_netcdf24", [](Fixture& f) {
+            std::unique_ptr<GDALDataset> recoded = wcallchecked(gdal::recode(f.dataset(), f.get_crop_area(), false, "MsatNetCDF24"));
+            f.check_cropped_image_data(recoded.get());
+        });
+    }
+};
+
+}
+}
+
+#endif
