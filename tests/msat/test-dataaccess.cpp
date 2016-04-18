2bacca17bb03188e68e75c38ff4b3aca432a6621
Enrico Zini <enrico@enricozini.org>
2016-04-18 15:24:15 +0200

    Ported more test

--- tests/msat/test-dataaccess.cpp
+++ tests/msat/test-dataaccess.cpp
@@ -1,28 +1,10 @@
-/*
- * Copyright (C) 2005--2010  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-
-#include "test-utils.h"
+#include <msat/utils/tests.h>
 #include <msat/xrit/dataaccess.h>
 #include <msat/xrit/fileaccess.h>
 #include <msat/hrit/MSG_HRIT.h>
 
 using namespace msat::xrit;
+using namespace msat::tests;
 
 namespace tut {
 
@@ -30,129 +12,121 @@ namespace tut {
 #define TESTDATA_HRV       DATA_DIR "/H:MSG1:HRV:200611141200"
 #define TESTDATA_NONLINEAR DATA_DIR "/H:MSG1:IR_039:200611130800"
 
-struct dataaccess_shar
+class Tests : public TestCase
 {
-        dataaccess_shar()
-        {
-        }
+    using TestCase::TestCase;
 
-        ~dataaccess_shar()
-        {
-        }
-};
-TESTGRP(dataaccess);
+    void register_tests() override;
+} test("msat_dataaccess");
 
-template<> template<>
-void to::test<1>()
+void Tests::register_tests()
 {
-        FileAccess fa(TESTDATA_LINEAR);
-        DataAccess da;
 
-        MSG_data pro;
-        MSG_data epi;
-        MSG_header header;
-        da.scan(fa, pro, epi, header);
+add_method("scan_linear", []() {
+    FileAccess fa(TESTDATA_LINEAR);
+    DataAccess da;
 
-        gen_ensure_equals(da.npixperseg, 3712u * 464u);
-        gen_ensure_equals(da.seglines, 464u);
-        gen_ensure_equals(da.swapX, true);
-        gen_ensure_equals(da.swapY, true);
-        gen_ensure_equals(da.hrv, false);
-        gen_ensure_equals(da.segnames.size(), 1u);
-        gen_ensure_equals(da.columns, 3712u);
-        gen_ensure_equals(da.lines, 3712u);
-        gen_ensure_equals(da.SouthLineActual, 1u);
-        gen_ensure_equals(da.WestColumnActual, 1u);
+    MSG_data pro;
+    MSG_data epi;
+    MSG_header header;
+    da.scan(fa, pro, epi, header);
 
-        gen_ensure_equals(da.line_start(   0), 0u);
-        gen_ensure_equals(da.line_start(1856), 0u);
-        gen_ensure_equals(da.line_start(3712), 0u);
-        gen_ensure_equals(da.line_start(9999), 0u);
+    wassert(actual(da.npixperseg) == 3712u * 464u);
+    wassert(actual(da.seglines) == 464u);
+    wassert(actual(da.swapX) == true);
+    wassert(actual(da.swapY) == true);
+    wassert(actual(da.hrv) == false);
+    wassert(actual(da.segnames.size()) == 1u);
+    wassert(actual(da.columns) == 3712u);
+    wassert(actual(da.lines) == 3712u);
+    wassert(actual(da.SouthLineActual) == 1u);
+    wassert(actual(da.WestColumnActual) == 1u);
 
-        gen_ensure(da.segment(0) != NULL);
+    wassert(actual(da.line_start(   0)) == 0u);
+    wassert(actual(da.line_start(1856)) == 0u);
+    wassert(actual(da.line_start(3712)) == 0u);
+    wassert(actual(da.line_start(9999)) == 0u);
 
-        MSG_SAMPLE buf[3712];
-        da.line_read(0, buf);
-}
+    wassert(actual(da.segment(0) != NULL).istrue());
 
-template<> template<>
-void to::test<2>()
-{
-        FileAccess fa(TESTDATA_NONLINEAR);
-        DataAccess da;
+    MSG_SAMPLE buf[3712];
+    wassert(da.line_read(0, buf));
+});
 
-        MSG_data pro;
-        MSG_data epi;
-        MSG_header header;
-        da.scan(fa, pro, epi, header);
+add_method("scan_nonlinear", []() {
+    FileAccess fa(TESTDATA_NONLINEAR);
+    DataAccess da;
 
-        gen_ensure_equals(da.npixperseg, 3712u * 464u);
-        gen_ensure_equals(da.seglines, 464u);
-        gen_ensure_equals(da.swapX, true);
-        gen_ensure_equals(da.swapY, true);
-        gen_ensure_equals(da.hrv, false);
-        gen_ensure_equals(da.segnames.size(), 1u);
-        gen_ensure_equals(da.columns, 3712u);
-        gen_ensure_equals(da.lines, 3712u);
-        gen_ensure_equals(da.SouthLineActual, 1u);
-        gen_ensure_equals(da.WestColumnActual, 1u);
+    MSG_data pro;
+    MSG_data epi;
+    MSG_header header;
+    da.scan(fa, pro, epi, header);
 
-        gen_ensure_equals(da.line_start(   0), 0u);
-        gen_ensure_equals(da.line_start(1856), 0u);
-        gen_ensure_equals(da.line_start(3712), 0u);
-        gen_ensure_equals(da.line_start(9999), 0u);
+    wassert(actual(da.npixperseg) == 3712u * 464u);
+    wassert(actual(da.seglines) == 464u);
+    wassert(actual(da.swapX) == true);
+    wassert(actual(da.swapY) == true);
+    wassert(actual(da.hrv) == false);
+    wassert(actual(da.segnames.size()) == 1u);
+    wassert(actual(da.columns) == 3712u);
+    wassert(actual(da.lines) == 3712u);
+    wassert(actual(da.SouthLineActual) == 1u);
+    wassert(actual(da.WestColumnActual) == 1u);
 
-        gen_ensure(da.segment(0) != NULL);
+    wassert(actual(da.line_start(   0)) == 0u);
+    wassert(actual(da.line_start(1856)) == 0u);
+    wassert(actual(da.line_start(3712)) == 0u);
+    wassert(actual(da.line_start(9999)) == 0u);
 
-        MSG_SAMPLE buf[3712];
-        da.line_read(0, buf);
-}
+    wassert(actual(da.segment(0) != NULL).istrue());
 
-template<> template<>
-void to::test<3>()
-{
-        FileAccess fa(TESTDATA_HRV);
-        DataAccess da;
+    MSG_SAMPLE buf[3712];
+    wassert(da.line_read(0, buf));
+});
 
-        MSG_data pro;
-        MSG_data epi;
-        MSG_header header;
-        da.scan(fa, pro, epi, header);
+add_method("scan_hrv", []() {
+    FileAccess fa(TESTDATA_HRV);
+    DataAccess da;
 
-        gen_ensure_equals(da.npixperseg, 11136u * 232u);
-        gen_ensure_equals(da.seglines, 464u);
-        gen_ensure_equals(da.swapX, true);
-        gen_ensure_equals(da.swapY, true);
-        gen_ensure_equals(da.hrv, true);
-        gen_ensure_equals(da.segnames.size(), 18u);
-        gen_ensure_equals(da.columns, 5568u);
-        gen_ensure_equals(da.lines, 11136u);
+    MSG_data pro;
+    MSG_data epi;
+    MSG_header header;
+    da.scan(fa, pro, epi, header);
 
-        gen_ensure_equals(da.LowerEastColumnActual, 1u);
-        gen_ensure_equals(da.LowerSouthLineActual, 1u);
-        gen_ensure_equals(da.LowerWestColumnActual, 5568u);
-        gen_ensure_equals(da.LowerNorthLineActual, 8128u);
-        gen_ensure_equals(da.UpperEastColumnActual, 2064u);
-        gen_ensure_equals(da.UpperSouthLineActual, 8129u);
-        gen_ensure_equals(da.UpperWestColumnActual, 7631u);
-        gen_ensure_equals(da.UpperNorthLineActual, 11136u);
+    wassert(actual(da.npixperseg) == 11136u * 232u);
+    wassert(actual(da.seglines) == 464u);
+    wassert(actual(da.swapX) == true);
+    wassert(actual(da.swapY) == true);
+    wassert(actual(da.hrv) == true);
+    wassert(actual(da.segnames.size()) == 18u);
+    wassert(actual(da.columns) == 5568u);
+    wassert(actual(da.lines) == 11136u);
 
-        gen_ensure_equals(da.line_start(    0), 11136u-7631u);
-        gen_ensure_equals(da.line_start( 1856), 11136u-7631u);
-        gen_ensure_equals(da.line_start( 3007), 11136u-7631u);
-        gen_ensure_equals(da.line_start( 3008), 11136u-5568u);
-        gen_ensure_equals(da.line_start( 9999), 11136u-5568u);
-        gen_ensure_equals(da.line_start(11135), 11136u-5568u);
-        gen_ensure_equals(da.line_start(11136), 0);
+    wassert(actual(da.LowerEastColumnActual) == 1u);
+    wassert(actual(da.LowerSouthLineActual) == 1u);
+    wassert(actual(da.LowerWestColumnActual) == 5568u);
+    wassert(actual(da.LowerNorthLineActual) == 8128u);
+    wassert(actual(da.UpperEastColumnActual) == 2064u);
+    wassert(actual(da.UpperSouthLineActual) == 8129u);
+    wassert(actual(da.UpperWestColumnActual) == 7631u);
+    wassert(actual(da.UpperNorthLineActual) == 11136u);
 
-        gen_ensure(da.segment(0) == NULL);
-        gen_ensure(da.segment(17) != NULL);
-        gen_ensure(da.segment(18) == NULL);
+    wassert(actual(da.line_start(    0)) == 11136u-7631u);
+    wassert(actual(da.line_start( 1856)) == 11136u-7631u);
+    wassert(actual(da.line_start( 3007)) == 11136u-7631u);
+    wassert(actual(da.line_start( 3008)) == 11136u-5568u);
+    wassert(actual(da.line_start( 9999)) == 11136u-5568u);
+    wassert(actual(da.line_start(11135)) == 11136u-5568u);
+    wassert(actual(da.line_start(11136)) == 0u);
 
-        MSG_SAMPLE buf[11136];
-        da.line_read(0, buf);
+    wassert(actual(da.segment(0) == NULL).istrue());
+    wassert(actual(da.segment(17) != NULL).istrue());
+    wassert(actual(da.segment(18) == NULL).istrue());
+
+    MSG_SAMPLE buf[11136];
+    wassert(da.line_read(0, buf));
+});
+
 }
 
 }
-
-/* vim:set ts=4 sw=4: */
