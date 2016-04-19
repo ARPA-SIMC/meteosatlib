dd4acf3d008ee46a782f0a7e528ecc717e0d22b6
Enrico Zini <enrico@enricozini.org>
2016-04-19 15:34:27 +0200

    Ported the rest of the tests

--- tests/gdal/test-xrit-reflectance.cpp
+++ tests/gdal/test-xrit-reflectance.cpp
@@ -1,112 +1,96 @@
-#include "test-utils.h"
-#include <stdint.h>
-
-#define PERFORM_SLOW_TESTS
+#include "utils.h"
+#include <cstdint>
 
 using namespace std;
+using namespace msat::tests;
 
-namespace tut {
+namespace {
 
-#define TESTFILE01  "H:MSG2:VIS006:200807150900"
-#define TESTFILE01r "H:MSG2:VIS006r:200807150900"
-#define TESTFILE04i "H:MSG1:IR_039r:200611130800"
-#define TESTFILE04  "H:MSG2:IR_039:201001191200"
-#define TESTFILE04r "H:MSG2:IR_039r:201001191200"
-#define TESTFILE12  "H:MSG1:HRVr:200611141200"
-
-struct xrit_reflectance_shar
+class Tests : public TestCase
 {
-    xrit_reflectance_shar()
-    {
-    }
+    using TestCase::TestCase;
 
-    ~xrit_reflectance_shar()
-    {
-    }
-};
-TESTGRP(xrit_reflectance);
+    void register_tests() override;
+} test("gdal_xrit_reflectance");
+
+void Tests::register_tests()
+{
 
 // Test opening channel 1 (VIS 0.6, with reflectance)
-template<> template<>
-void to::test<1>()
-{
-    unique_ptr<GDALDataset> dataset = openro(TESTFILE01);
-    gen_ensure(dataset.get() != 0);
-    gen_ensure_equals(string(GDALGetDriverShortName(dataset->GetDriver())), "MsatXRIT");
+add_method("vis06", []{
+    unique_ptr<GDALDataset> dataset = gdal::open_ro("H:MSG2:VIS006:200807150900");
+    wassert(actual(dataset.get() != 0).istrue());
+    wassert(actual(GDALGetDriverShortName(dataset->GetDriver())) == "MsatXRIT");
 
     // Check that we have the real and the virtual raster bands
-    gen_ensure_equals(dataset->GetRasterCount(), 1);
+    wassert(actual(dataset->GetRasterCount()) == 1);
 
     // x:2000,y:3400
     GDALRasterBand* rb = dataset->GetRasterBand(1);
     uint16_t val;
     rb->RasterIO(GF_Read, 2000, 3400, 1, 1, &val, 1, 1, GDT_UInt16, 0, 0);
-    gen_ensure_equals(val, 96);
+    wassert(actual(val) == 96);
 
-    unique_ptr<GDALDataset> datasetr = openro(TESTFILE01r);
-    gen_ensure(datasetr.get() != 0);
-    gen_ensure_equals(string(GDALGetDriverShortName(datasetr->GetDriver())), "MsatXRIT");
+    unique_ptr<GDALDataset> datasetr = gdal::open_ro("H:MSG2:VIS006r:200807150900");
+    wassert(actual(dataset.get() != 0).istrue());
+    wassert(actual(string(GDALGetDriverShortName(datasetr->GetDriver()))) == "MsatXRIT");
 
     // Check that we have the real and the virtual raster bands
-    gen_ensure_equals(datasetr->GetRasterCount(), 1);
+    wassert(actual(datasetr->GetRasterCount()) == 1);
 
     rb = datasetr->GetRasterBand(1);
     float valr;
     rb->RasterIO(GF_Read, 2000, 3400, 1, 1, &valr, 1, 1, GDT_Float32, 0, 0);
-    gen_ensure_similar(valr, 25.9648, 0.0001);
-}
+    wassert(actual((double)valr).almost_equal(25.9648, 3));
+});
 
 // Test opening channel 4 (IR 0.39, with missing accessory channels)
-template<> template<>
-void to::test<2>()
-{
+add_method("ir039_missing", []{
     try {
-        unique_ptr<GDALDataset> dataset = openro(TESTFILE04i);
-        gen_ensure(false);
+        unique_ptr<GDALDataset> dataset = gdal::open_ro("H:MSG1:IR_039r:200611130800");
+        wassert(actual(false).istrue());
     } catch (...) {
     }
-}
+});
 
 // Test opening channel 4 (IR 0.39, with all accessory channels yet)
-template<> template<>
-void to::test<3>()
-{
-    unique_ptr<GDALDataset> dataset = openro(TESTFILE04);
-    gen_ensure(dataset.get() != 0);
-    gen_ensure_equals(string(GDALGetDriverShortName(dataset->GetDriver())), "MsatXRIT");
+add_method("ir039", []{
+    unique_ptr<GDALDataset> dataset = gdal::open_ro("H:MSG2:IR_039:201001191200");
+    wassert(actual(dataset.get() != 0).istrue());
+    wassert(actual(string(GDALGetDriverShortName(dataset->GetDriver()))) == "MsatXRIT");
 
     // Check that we have the real and the virtual raster bands
-    gen_ensure_equals(dataset->GetRasterCount(), 1);
+    wassert(actual(dataset->GetRasterCount()) == 1);
 
     // x:2000,y:350
     GDALRasterBand* rb = dataset->GetRasterBand(1);
     uint16_t val;
     rb->RasterIO(GF_Read, 2000, 350, 1, 1, &val, 1, 1, GDT_UInt16, 0, 0);
-    gen_ensure_equals(val, 287);
+    wassert(actual(val) == 287);
 
-    unique_ptr<GDALDataset> datasetr = openro(TESTFILE04r);
-    gen_ensure(datasetr.get() != 0);
-    gen_ensure_equals(string(GDALGetDriverShortName(datasetr->GetDriver())), "MsatXRIT");
+    unique_ptr<GDALDataset> datasetr = gdal::open_ro("H:MSG2:IR_039r:201001191200");
+    wassert(actual(dataset.get() != 0).istrue());
+    wassert(actual(string(GDALGetDriverShortName(datasetr->GetDriver()))) == "MsatXRIT");
 
     // Check that we have the real and the virtual raster bands
-    gen_ensure_equals(datasetr->GetRasterCount(), 1);
+    wassert(actual(datasetr->GetRasterCount()) == 1);
 
     rb = datasetr->GetRasterBand(1);
     float valr;
     rb->RasterIO(GF_Read, 2000, 350, 1, 1, &valr, 1, 1, GDT_Float32, 0, 0);
-    gen_ensure_similar(valr, 22.3242, 0.0001);
-}
+    wassert(actual((double)valr).almost_equal(22.3242, 3));
+});
 
 // Test opening channel 12 (HRV, with reflectance)
-template<> template<>
-void to::test<4>()
-{
-    unique_ptr<GDALDataset> dataset = openro(TESTFILE12);
-    gen_ensure(dataset.get() != 0);
-    gen_ensure_equals(string(GDALGetDriverShortName(dataset->GetDriver())), "MsatXRIT");
+add_method("hrv", []{
+    unique_ptr<GDALDataset> dataset = gdal::open_ro("H:MSG1:HRVr:200611141200");
+    wassert(actual(dataset.get() != 0).istrue());
+    wassert(actual(string(GDALGetDriverShortName(dataset->GetDriver()))) == "MsatXRIT");
 
     // Check that we have the real and the virtual raster bands
-    gen_ensure_equals(dataset->GetRasterCount(), 1);
+    wassert(actual(dataset->GetRasterCount()) == 1);
+});
+
 }
 
 }
