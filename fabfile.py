db9942c599ecda1094a5ab74845a16ab584e82fb
Enrico Zini <enrico@enricozini.org>
2020-11-09 16:19:09 +0100

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,103 +2,71 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
-from fabric.api import local, run, sudo, cd, env, hosts, shell_env
-from fabric.contrib.files import exists
+from fabric.api import local, run, cd, env, hosts, put
+from io import BytesIO
 import git
 import re
 from six.moves import shlex_quote
 
-env.hosts = ["venti", "ventiquattro"]
+env.hosts = ["venti", "ventiquattro", "sette", "otto"]
 env.use_ssh_config = True
+
 
 def cmd(*args):
     return " ".join(shlex_quote(a) for a in args)
 
-@hosts("venti")
-def test_venti():
-    fedora_cxxflags = "-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong"\
-                      " --param=ssp-buffer-size=4 -grecord-gcc-switches  -m64 -mtune=generic"
-    fedora_ldflags = "-Wl,-z,relro"
 
+def configure():
+    res = run(cmd("rpm", "--eval", "%configure --enable-arpae-tests"))
+    put(BytesIO(b"\n".join(res.stdout.splitlines())), "rpm-config")
+    run(cmd("chmod", "0755", "rpm-config"))
+    run(cmd("./rpm-config"))
+
+
+def push(host):
     repo = git.Repo()
-    remote = repo.remote("venti")
+    remote = repo.remote(host)
     push_url = remote.config_reader.get("url")
     remote_dir = re.sub(r"^ssh://[^/]+", "", push_url)
 
-    local(cmd("git", "push", "venti", "HEAD"))
+    local(cmd("git", "push", host, "HEAD", "--force"))
     with cd(remote_dir):
-        #run(cmd("git", "fetch"))
-        run(cmd("git", "checkout", "-B", "test_venti", repo.head.commit.hexsha))
         run(cmd("git", "reset", "--hard"))
         run(cmd("git", "clean", "-fx"))
-        run(cmd("cp", "../publicdecompwt/PublicDecompWT-2.06.zip", "decompress/PublicDecompWT.zip"))
+        run(cmd("git", "checkout", "-B", "test_" + host, repo.head.commit.hexsha))
+        run(cmd("git", "clean", "-fx"))
         run(cmd("autoreconf", "-if"))
-        run(cmd("./configure",
-                "--build=x86_64-redhat-linux-gnu",
-                "--host=x86_64-redhat-linux-gnu",
-                "--program-prefix=",
-                "--prefix=/usr",
-                "--exec-prefix=/usr",
-                "--bindir=/usr/bin",
-                "--sbindir=/usr/sbin",
-                "--sysconfdir=/etc",
-                "--datadir=/usr/share",
-                "--includedir=/usr/include",
-                "--libdir=/usr/lib64",
-                "--libexecdir=/usr/libexec",
-                "--localstatedir=/var",
-                "--sharedstatedir=/var/lib",
-                "--mandir=/usr/share/man",
-                "--infodir=/usr/share/info",
-                "CFLAGS=" + fedora_cxxflags,
-                "CXXFLAGS=" + fedora_cxxflags,
-                "LDFLAGS=" + fedora_ldflags))
-        run(cmd("make"))
-        run(cmd("make", "check"))
+        configure()
+
+
+def run_test(host):
+    push(host)
+
+    repo = git.Repo()
+    remote = repo.remote(host)
+    push_url = remote.config_reader.get("url")
+    remote_dir = re.sub(r"^ssh://[^/]+", "", push_url)
+    with cd(remote_dir):
+        run(cmd("git", "submodule", "update", "--init", "--recursive"))
+        run(cmd("make", "-j2"))
+        run(cmd("make", "check", "-j2", "TEST_VERBOSE=1"))
+
+
+@hosts("venti")
+def test_venti():
+    run_test("venti")
+
 
 @hosts("ventiquattro")
 def test_ventiquattro():
-    fedora_cxxflags = "-O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong"\
-                      " --param=ssp-buffer-size=4 -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -m64 -mtune=generic"
-    fedora_ldflags = "-Wl,-z,relro -specs=/usr/lib/rpm/redhat/redhat-hardened-ld"
-
-    repo = git.Repo()
-    remote = repo.remote("ventiquattro")
-    push_url = remote.config_reader.get("url")
-    remote_dir = re.sub(r"^ssh://[^/]+", "", push_url)
-
-    local(cmd("git", "push", "ventiquattro", "HEAD"))
-    with cd(remote_dir):
-        run(cmd("git", "checkout", "-B", "test_venti", repo.head.commit.hexsha))
-        run(cmd("git", "reset", "--hard"))
-        run(cmd("git", "clean", "-fx"))
-        run(cmd("cp", "../../publicdecompwt/PublicDecompWT-2.06.zip", "decompress/PublicDecompWT.zip"))
-        run(cmd("autoreconf", "-if"))
-        run(cmd("./configure",
-                "--build=x86_64-redhat-linux-gnu",
-                "--host=x86_64-redhat-linux-gnu",
-                "--program-prefix=",
-                "--disable-dependency-tracking",
-                "--prefix=/usr",
-                "--exec-prefix=/usr",
-                "--bindir=/usr/bin",
-                "--sbindir=/usr/sbin",
-                "--sysconfdir=/etc",
-                "--datadir=/usr/share",
-                "--includedir=/usr/include",
-                "--libdir=/usr/lib64",
-                "--libexecdir=/usr/libexec",
-                "--localstatedir=/var",
-                "--sharedstatedir=/var/lib",
-                "--mandir=/usr/share/man",
-                "--infodir=/usr/share/info",
-                "CFLAGS=" + fedora_cxxflags,
-                "CXXFLAGS=" + fedora_cxxflags,
-                "LDFLAGS=" + fedora_ldflags))
-        run(cmd("make"))
-        run(cmd("make", "check"))
+    run_test("ventiquattro")
 
 
-def test():
-    test_venti()
-    test_ventiquattro()
+@hosts("sette")
+def test_sette():
+    run_test("sette")
+
+
+@hosts("otto")
+def test_otto():
+    run_test("otto")
